2a68dce82d3f69589de577d110caecfa188a4863
Reese Morgan <reese.morgan@morris.com>
2017-10-30 15:23:57 -0400

    remove last idea at an application.

--- web/index.php
+++ web/index.php
@@ -4,7 +4,8 @@ ini_set('display_errors', 0);
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-$app = require __DIR__.'/../app/app.php';
+//$app = require __DIR__.'/../app/app.php';
+$app = new Sandbox\Application();
 require __DIR__.'/../config/prod.php';
 require __DIR__.'/../app/controllers.php';
 $app['debug'] = true;
